4a944592378dad6eaa2d2e7c6e825d186817dcae
jaimecuellar14 <jamescuellar14@gmail.com>
2021-02-06 16:21:35 +0100

    Home screen designed

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,7 +1,40 @@
+import jaime from "../Assets/jaime2.jpg";
+import "../styles/home.css"
+const { Container } = require("@material-ui/core")
 const Home = () =>{
     return(
-        <div>
-            Home Component
+        <div className="home-container">
+            <Container maxWidth="sm" className="home-info">
+                <div className="item">
+                    <a href="/about">
+                        <h1 className="label hi new-label">
+                            <span>
+                                Hi, I am
+                            </span>
+                        </h1>
+                    </a>
+                </div>
+                <div className="item">
+                    <a href="/contact">
+                        <h2 className="label jaime new-label">
+                            <span>
+                                Jaime
+                            </span>
+                        </h2>
+                    </a>
+                </div>
+                <div className="item">
+                    <a href="/work">
+                        <h3 className="label cuellar new-label">
+                            <span>
+                                Cuellar
+                            </span>
+                        </h3>
+                    </a>
+                </div>
+            </Container>
+            <Container maxWidth="sm" className="home-picture">
+            </Container>
         </div>
     )
 }
