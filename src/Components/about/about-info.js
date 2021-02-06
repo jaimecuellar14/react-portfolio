3e26c6f033408bf0401397d54cacba81ac8317e7
jaimecuellar14 <jamescuellar14@gmail.com>
2021-02-06 16:57:15 +0100

    creation of about page

---
+++ src/Components/about/about-info.js
@@ -0,0 +1,12 @@
+const AboutInfo = () => {
+    return(
+        <div className="about-info">
+            <p>
+                I'm an informatic engineer from the European University of the Atlantic.
+                
+            </p>
+        </div>
+    )
+}
+
+export default AboutInfo;
