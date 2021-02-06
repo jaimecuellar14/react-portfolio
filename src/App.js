4fd1b69ac7ba1b2d580a2a3fedfc0970abb5923a
jaimecuellar14 <jamescuellar14@gmail.com>
2021-02-06 15:07:40 +0100

    Material UI installed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
