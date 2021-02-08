7702d7bd319e9361edda9ac6e4cbec163f6464c5
jaimecuellar14 <jamescuellar14@gmail.com>
2021-02-08 12:00:18 +0100

    Iniciamos vista de trabajos

---
+++ src/Components/work/work-loading-spinner.js
@@ -0,0 +1,11 @@
+import { CircularProgress } from '@material-ui/core';
+
+const WorkLoadingSpinner = () =>{
+    return(
+        <div className="loading-spinner">
+            <CircularProgress/>
+        </div>
+    )
+}
+
+export default WorkLoadingSpinner;
--- src/Views/Work.js
+++ src/Views/Work.js
@@ -1,4 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import WorkLoadingSpinner from '../Components/work/work-loading-spinner';
+
 const Work = () => {
+
+    const [ repos , setRepos] = useState();
+    const [ loadingRepos, setLoadingRepos ] = useState(false);
+
+    const getRepositories = () =>{
+        setLoadingRepos(true);
+        console.log(process.env.REACT_APP_GITHUB_TOKEN);
+        fetch("https://api.github.com/users/jaimecuellar14/repos?per_page=50",{
+            method:"GET",
+            headers:{
+                Authorization : `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+        })
+            .then(response => response.json())
+            .then(data=>{
+                setLoadingRepos(false)
+                setRepos();
+                console.log(data);
+            })
+            .catch(error=>console.log(error));
+    }
+    useEffect(()=>{
+        getRepositories();
+    },[]);
+
+    if (loadingRepos){
+        return(<WorkLoadingSpinner/>);
+    }
     return(
         <div>
             Work component
